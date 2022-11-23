cbe02d4695cadd849c2fe0c3faed19bc4a6c1d81
Christine Parry <christine.parry9@gmail.com>
2022-11-23 17:09:08 +0100

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+Restaurant.destroy_all
+
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "i am a restaurant"
+  5.times do
+    review = Review.new(
+      content: Faker::Quote.famous_last_words,
+      rating: [0, 1, 2, 3, 4, 5].sample
+    )
+  restaurant.save!
+  review.restaurant = restaurant
+  review.save!
+  puts "i am a review"
+
+  end
+
+end
